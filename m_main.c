d8beaa756d5d6c2d128a53ca9c57897e2beadbac
bamansour1978 <karatman@gmail.com>
2023-06-24 05:55:49 +0100

    Update

--- m_main.c
+++ m_main.c
@@ -19,11 +19,11 @@ int main(int argc, char **argv)
 	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-		return (usage_error());
+		return (m_usage_err());
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
-		return (f_open_error(argv[1]));
-	exit_code = run_monty(script_fd);
+		return (file_open_err(argv[1]));
+	exit_code = runn_monty(script_fd);
 	fclose(script_fd);
 	return (exit_code);
 }
