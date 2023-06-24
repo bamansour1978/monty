3713b1c179d46cbec78addcf68d0f1ce201e8079
bamansour1978 <karatman@gmail.com>
2023-06-24 05:52:08 +0100

    Update

--- m_main.c
+++ m_main.c
@@ -1,4 +1,3 @@
-
 #include "monty.h"
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,11 +19,11 @@ int main(int argc, char **argv)
 	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-		return (m_usage_err());
+		return (usage_error());
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
-		return (file_open_err(argv[1]));
-	exit_code = runn_monty(script_fd);
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
 	fclose(script_fd);
 	return (exit_code);
 }
